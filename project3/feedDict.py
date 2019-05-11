eccf6747e60b2c0347f635bb9a5634da9d2e5d68
Bhavik Kawli <kawli.bhavik88@gmail.com>
2019-05-10 23:42:11 -0700

    added examples

---
+++ project3/feedDict.py
@@ -0,0 +1,13 @@
+# Quiz Solution
+import tensorflow as tf
+
+def run():
+    output = None
+    x = tf.placeholder(tf.int32)
+
+    with tf.Session() as sess:
+        output = sess.run(x, feed_dict={x: 123})
+
+    return output
+
+run()
